cd0aa8b225c4dab4207951160646d5d3b9d855b3
Vaibhav <vaibhavsinh@gmail.com>
2015-07-13 01:18:01 +0530

    Removed older files

--- admin/src/main/java/com/pgms/admin/controller/AdminController.java
+++ admin/src/main/java/com/pgms/admin/controller/AdminController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by user-1 on 27/6/15.
  */
 @Controller
-/*@RequestMapping("/ui")*/
 public class AdminController {
 
     @RequestMapping("/login")
@@ -15,58 +14,10 @@ public class AdminController {
         return "login";
     }
 
-    @RequestMapping("/home")
-    public String admin() {
-        return "admin";
-    }
-
     @RequestMapping("/")
     public String index() {
         return "index";
     }
-
-    @RequestMapping("/designation")
-    public String designation() {
-        return "designation";
-    }
-
-    @RequestMapping("/complaintStatus")
-    public String complaintStatus() {
-        return "complaintStatus";
-    }
-
-    @RequestMapping("/verificationStatus")
-    public String verificationStatus() {
-        return "verificationStatus";
-    }
-
-    @RequestMapping("/reviewStatus")
-    public String reviewStatus() {
-        return "reviewStatus";
-    }
-
-    @RequestMapping("/category")
-    public String category() {
-        return "category1";
-    }
-
-    @RequestMapping("/location")
-    public String location() {
-        return "location";
-    }
-
-    @RequestMapping("/department")
-    public String department() {
-        return "department";
-    }
-
-    @RequestMapping("/officer")
-    public String officer() {
-        return "officer";
-    }
-
-
-
 
     @RequestMapping("/partials/category")
     public String partialCategory() {
